7fe3872640386349a22f4dd62e0048818cd79820
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-08-28 17:50:09 +0200

    fixed syntax and formatting in s3

--- scripts3.js
+++ scripts3.js
@@ -1,8 +1,28 @@
+// const leoName = "Leo";
+// const leoSurname = "Musvaire     ";
+// const leoBalance = "-9394";
+
+// const sarahName = "Sarah    ";
+// const sarahSurname = "Kleinhans";
+// const sarahBalance = "-4582.2";
+
+// const divider = "----------------------------------";
+
+// // Only change below this line
+
+// const owed = parseInt("R" + leoBalance + sarahBalance);
+// const leo = '${leoName} + {leoSurname} + "Owed" + "R" + {sarahBalance}';
+// const sarah = '{leoName} + {surname} + "Owed" + "R" + {sarahBalance}';
+// const total = "Total amount owed: ";
+// const result = leo + sarah + divider + divider + total + owed + divider;
+
+// console.log(result);
+
 const leoName = "Leo";
-const leoSurname = "Musvaire     ";
+const leoSurname = "Musvaire";
 const leoBalance = "-9394";
 
-const sarahName = "Sarah    ";
+const sarahName = "Sarah";
 const sarahSurname = "Kleinhans";
 const sarahBalance = "-4582.2";
 
@@ -10,10 +30,10 @@ const divider = "----------------------------------";
 
 // Only change below this line
 
-const owed = parseInt("R" + leoBalance + sarahBalance);
-const leo = '{leoName} + {leoSurname} + "Owed" + "R" + {sarahBalance}';
-const sarah = '{leoName} + {surname} + "Owed" + "R" + {sarahBalance}';
-const total = "Total amount owed: ";
-const result = leo + sarah + divider + divider + total + owed + divider;
+const owed = parseInt(`R ${leoBalance} ${sarahBalance}`);
+const leo = `${leoName} ${leoSurname} (Owed: R${leoBalance})\n`;
+const sarah = `${sarahName} ${sarahSurname} (Owed: R${sarahBalance})\n`;
+const total = `Total amount owed:`;
+const result = `${leo} ${sarah} ${divider} ${total}  ${owed} + ${divider}`;
 
 console.log(result);
