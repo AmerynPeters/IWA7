b2dd84d702a4f8349423b4878afd2ae5dedf1fc1
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-08-28 16:31:15 +0200

    completed challenge 1

---
+++ scripts1.js
@@ -0,0 +1,4 @@
+const value = 3;
+console.log(value + 4 + value);
+// Why it doesn't work: 3 is assigned as a string instead of a numerical, 
+//so it will be concatenated as a string instead of added to the number 4 using the + operator.
